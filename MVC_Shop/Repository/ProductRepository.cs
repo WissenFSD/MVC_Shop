858465b634dca8f1fc5d0f403d145aa5cede8b9b
wissenfsd@gmail.com <wissenfsd@gmail.com>
2024-03-30 11:56:46 +0300

    kdkfjdfdf

--- MVC_Shop/Repository/ProductRepository.cs
+++ MVC_Shop/Repository/ProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using MVC_Shop.DMO;
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_Shop.DMO;
 using MVC_Shop.Models.DTO;
 
 namespace MVC_Shop.Repository
@@ -7,7 +8,7 @@ namespace MVC_Shop.Repository
 	{
 		public List<ProductDTO> GetProductBySubCategoryId(int subCategoryId);
 	}
-	public class ProductRepository:IProductRepository
+	public class ProductRepository : IProductRepository
 	{
 		private AdventureWorks2019Context _context;
 		public ProductRepository(AdventureWorks2019Context context)
@@ -16,12 +17,20 @@ namespace MVC_Shop.Repository
 		}
 		public List<ProductDTO> GetProductBySubCategoryId(int subCategoryId)
 		{
-			var result = _context.Products.Where(s => s.ProductSubcategoryId == subCategoryId).Select(k => new ProductDTO()
+			var result = _context.Products.Include(m => m.ProductProductPhotos).ThenInclude(k => k.ProductPhoto).Where(s => s.ProductSubcategoryId == subCategoryId).Select(k => new ProductDTO()
 			{
 				Id = k.ProductId,
 				Name = k.Name,
-				Price = k.ListPrice
+				Price = k.ListPrice,
+				Photo = k.ProductProductPhotos.Where(s => s.ProductId == s.ProductId).Select(k => k.ProductPhoto.LargePhoto).SingleOrDefault()
 			}).ToList();
+
+
+			//List<int> ids = result.Select(s => s.Id).ToList();
+			//// photo çekelim 
+
+			//List<byte[]> photos = _context.ProductProductPhotos.Include(s => s.ProductPhoto).Where(s => ids.Contains(s.ProductId)).Select(k => k.ProductPhoto.LargePhoto).ToList();
+
 
 			return result;
 		}
